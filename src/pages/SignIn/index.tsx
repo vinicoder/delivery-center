9b17140d1afb5499479ebbaf1dcfb46b8a2fff6b
Vinicius de Moraes <vinicoder@gmail.com>
2020-09-15 16:07:13 -0300

    feat: new page signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,12 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from 'hooks/AuthContext';
 
-import figureSignin from '../../assets/figure-signin.svg';
-import logo from '../../assets/logo.png';
-import Input from '../../components/Form/Input';
-import Button from '../../components/Button';
+import figureSignin from 'assets/figure-signin.svg';
+import logo from 'assets/logo.png';
+import Input from 'components/Form/Input';
+import Button from 'components/Button';
 
 import { Container } from './styles';
 
@@ -19,6 +19,16 @@ const SignIn: React.FC = () => {
     signIn({ email });
   }
 
+  useEffect(() => {
+    const body = document.querySelector('body');
+
+    if (body) body.classList.add('signin');
+
+    return () => {
+      if (body) body.classList.remove('signin');
+    };
+  }, []);
+
   return (
     <Container className="container">
       <div className="row">
@@ -27,6 +37,8 @@ const SignIn: React.FC = () => {
             src={figureSignin}
             alt="Entregador segurando caixas para entregar"
             className="img-fluid"
+            width={420}
+            height={420}
           />
         </div>
         <div className="col">
@@ -47,7 +59,6 @@ const SignIn: React.FC = () => {
                 name="email"
                 type="email"
                 label="E-mail"
-                autoComplete="none"
                 onChange={({ target }) => setEmail(target.value)}
                 required
               />
@@ -56,10 +67,9 @@ const SignIn: React.FC = () => {
                 name="password"
                 type="password"
                 label="Senha"
-                autoComplete="new-password"
                 required
               />
-              <Button type="submit" iconRight={FiArrowRight}>
+              <Button type="submit" icon={FiArrowRight} iconPosition="right">
                 Entrar
               </Button>
             </form>
