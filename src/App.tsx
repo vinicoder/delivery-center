c9b465ecf6a035d21df90c65115534eb84c7816f
Vinicius de Moraes <vinicoder@gmail.com>
2020-09-16 04:41:40 -0300

    feat: graphql configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from 'utils/ScrollToTop';
+import { ApolloProvider } from '@apollo/client';
 import { AuthProvider } from './hooks/AuthContext';
 
 import Routes from './routes';
 
 import GlobalStyles from './styles/global';
 
+import graphqlClient from './services/graphqlClient';
+
 const App: React.FC = () => (
-  <AuthProvider>
-    <BrowserRouter>
-      <Routes />
-      <ScrollToTop />
-    </BrowserRouter>
-    <GlobalStyles />
-  </AuthProvider>
+  <ApolloProvider client={graphqlClient}>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes />
+        <ScrollToTop />
+      </BrowserRouter>
+      <GlobalStyles />
+    </AuthProvider>
+  </ApolloProvider>
 );
 
 export default App;
